4a0571481164e491318f2531365074878b6b859c
Alexandre Chêne <hello@kooparse.com>
2023-02-13 14:35:39 +0100

    Update to zig 0.11.x

--- build.zig
+++ build.zig
@@ -2,10 +2,19 @@ const std = @import("std");
 const Builder = std.build.Builder;
 
 pub fn build(b: *Builder) void {
-    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
 
-    var tests = b.addTest("src/main.zig");
-    tests.setBuildMode(mode);
+    _ = b.addModule(.{
+        .name = "texture-packer",
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
+    var tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
 
     const test_step = b.step("test", "Run tests");
     test_step.dependOn(&tests.step);
